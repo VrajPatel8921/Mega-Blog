231663ce7cafd27d95b9b7503c24c684a4be070a
Vraj Patel <patelvraj892.com>
2024-10-05 17:19:27 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+// import { Login as loginComponent } from "../components";
+// react componet name always starts with capital letters
+
+import { Login as LoginComponent } from "../components";
+
+function Login() {
+  return (
+    <div className="py-8">
+      <LoginComponent />
+    </div>
+  );
+}
+
+export default Login;
